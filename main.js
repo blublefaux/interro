1fd5348f5871f72afa524a6b80a0a07ed565e5b9
Liam Vinckbooms <liam.vinckbooms@cesjb.be>
2025-11-20 15:57:04 +0100

    aled

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-s
+import Etudiant from Etudiant.js
+var etudiant1= new Etudiant ("pierre",32,"info")
+var etudiant2= new Etudiant ("arhur", 47 ,"maternelle")
+var etudiant3= new Etudiant ("le bg(liam)", 7 ,"le meileur")
